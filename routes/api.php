52e3315059ccdf95eb6c8538120942ac91347456
NaelleNangmo <naelle@camwater.cm>
2026-05-25 14:41:52 +0100

    refactor(api): nettoyer les routes inutilisees

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@ use App\Http\Controllers\AuthLivreurController;
 use App\Http\Controllers\LivraisonController;
 use Illuminate\Support\Facades\Route;
 
+// Auth
 Route::post("/auth/login",  [AuthLivreurController::class, "login"]);
+
+// Routes protegees
 Route::middleware("auth:sanctum")->group(function () {
     Route::post("/auth/logout", [AuthLivreurController::class, "logout"]);
-    Route::apiResource("livraisons", LivraisonController::class);
+
+    // Livraisons  CRUD + suivi
+    Route::apiResource("livraisons", LivraisonController::class)
+         ->only(["index", "store", "show"]);
     Route::get("livraisons/{livraison}/suivi", [LivraisonController::class, "suivi"]);
 });
